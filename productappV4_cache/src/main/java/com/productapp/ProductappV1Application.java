8069b6e4b06515b73a590e22033728e21e14bb49
Kshitij Thatte <kshitijthatte@gmail.com>
2022-08-21 10:10:48 +0530

    completed assignment

--- productappV4_cache/src/main/java/com/productapp/ProductappV1Application.java
+++ productappV4_cache/src/main/java/com/productapp/ProductappV1Application.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.productapp.repository.Product;
@@ -14,14 +15,15 @@ import com.productapp.repository.ProductDao;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@EnableScheduling	//1. put enable cache annotation
-@OpenAPIDefinition(info = @Info(title = "Oracle Product API", version = "2.0" , description = "Oracle product API"))
+@EnableScheduling // 1. put enable cache annotation
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "Oracle Product API", version = "2.0", description = "Oracle product API"))
 @SpringBootApplication
-public class ProductappV1Application implements CommandLineRunner{
+public class ProductappV1Application implements CommandLineRunner {
 
 	@Autowired
 	private ProductDao productDao;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProductappV1Application.class, args);
 	}
@@ -30,7 +32,7 @@ public class ProductappV1Application implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		productDao.save(new Product("laptop", new BigDecimal(100000)));
 		productDao.save(new Product("printer", new BigDecimal(10000)));
-		
+
 	}
 
 }
